ef0d66a541bf7419e5737b6fccbf60bd28ab2ffb
CaoTriNguyen <caotri.0696@gmail.com>
2023-10-26 14:29:31 +0700

    fetch api from server localhost

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './Home.module.scss';
@@ -5,6 +6,29 @@ import styles from './Home.module.scss';
 const cx = classNames.bind(styles);
 
 function Home() {
+    useEffect(() => {
+        // fetch(`http://localhost:5000/api`, {
+        //     mode: 'no-cors',
+        //     method: 'GET',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        // })
+        fetch(`http://localhost:5000/api`,{ 
+            method: 'GET',
+        })
+            .then((response) => {
+                console.log(response);
+                if (!response.ok) {
+                    throw new Error(`This is an HTTP error: The status is ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((actualData) => console.log(actualData))
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
     return <div className={cx('wrapper')}>Home</div>;
 }
 
