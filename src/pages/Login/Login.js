97e83867439211eba44ce76bb5b7e6e244fd12fb
CaoTriNguyen <caotri.0696@gmail.com>
2023-11-17 17:49:38 +0700

    update login UI

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 
 import Button from '~/components/Button';
+import { EyeSlashIcon, EyeIcon, LockIcon, UserIcon } from '~/components/icons';
 
 import styles from './Login.module.scss';
-import { LockIcon, UserIcon } from '~/components/icons';
 
 const cx = classNames.bind(styles);
 
 function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+    const [password, setPassword] = useState('');
+
+    const handleInputPassword = (event) => {
+        setPassword(event.target.value);
+    };
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('login')}>
@@ -15,11 +23,21 @@ function Login() {
 
                 <div className={cx('item')}>
                     <UserIcon className={cx('icon')} />
-                    <input className={cx('username')} />
+                    <input className={cx('username')} placeholder="Username" />
                 </div>
                 <div className={cx('item')}>
                     <LockIcon className={cx('icon')} />
-                    <input className={cx('password')} />
+                    <input
+                        placeholder="Password"
+                        type={showPassword ? 'text' : 'password'}
+                        value={password}
+                        onChange={handleInputPassword}
+                        className={cx('password')}
+                    />
+                    <div className={cx('password-status')} onClick={() => setShowPassword(!showPassword)}>
+                        {!showPassword && <EyeIcon />}
+                        {showPassword && <EyeSlashIcon />}
+                    </div>
                 </div>
                 <Button className={cx('login-btn')}>Login</Button>
                 <a href="./signup" className={cx('forgot-password')}>
