324b04618b6ef29b822a68f6547de3ebc9097672
CaoTriNguyen <caotri.0696@gmail.com>
2023-11-20 11:53:13 +0700

    Update Theme

--- src/components/DarkMode/DarkMode.js
+++ src/components/DarkMode/DarkMode.js
@@ -9,7 +9,9 @@ const cx = classNames.bind(styles);
 function DarkMode() {
     // eslint-disable-next-line
     const [state, dispatch] = useTheme();
-    const [darkTheme, setDarkTheme] = useState(state.theme === 'dark');
+    const [darkTheme, setDarkTheme] = useState(localStorage.theme === 'dark');
+    document.querySelector('body').setAttribute('data-theme', localStorage.theme);
+
     return (
         <div
             className={cx('wrapper')}
@@ -17,6 +19,7 @@ function DarkMode() {
                 await dispatch(actions.setTodoTheme(darkTheme ? 'light' : 'dark'));
                 setDarkTheme(state.theme === 'dark');
                 document.querySelector('body').setAttribute('data-theme', state.theme);
+                localStorage.theme = state.theme;
             }}
         >
             {darkTheme && <SunIcon className={cx('icon')} />}
